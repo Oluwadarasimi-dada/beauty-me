ff30c89cd323fc42302ffe68d8ada1c9a78bd4e6
Oluwadarasimi-dada <dadadaniels321@gmail.com>
2025-09-30 14:02:37 +0100

    Add files via upload

---
+++ proect.js
@@ -0,0 +1,16 @@
+const treatments = () =>{
+
+    const d = document.getElementById("dan")
+
+
+if (d.textContent==="CONTACT US")
+     
+    {d.textContent="+234 90875623456"}
+
+else{d.textContent="CONTACT US"
+    
+}
+
+
+}
+
